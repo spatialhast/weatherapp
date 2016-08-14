55079c8e0bd72663f9ee631c0b8a8deafd860c51
spatialhast <spatial.hast@gmail.com>
2016-08-14 11:51:15 +0300

    add appComponent

--- app/main.js
+++ app/main.js
@@ -1,1 +1,8 @@
-  
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_component_1 = require('./appComponent/app.component');
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
+//, [
+//   appRouterProviders
+//])
+//# sourceMappingURL=main.js.map
---
+++ app/main.ts
@@ -0,0 +1,11 @@
+import { bootstrap } from '@angular/platform-browser-dynamic'
+
+import { AppComponent } from './appComponent/app.component'
+
+
+
+bootstrap(AppComponent);
+//, [
+ //   appRouterProviders
+//])
+
